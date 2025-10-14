a92b813074822454832b0364049a81d81a524c17
Trésor Ndayikeza <160187183+kiddocoder@users.noreply.github.com>
2025-10-15 00:11:30 +0200

    add initial theme and color constants, and create empty feature files

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const accentColor = Color(0xFF65B947);
+const primaryColor = Color(0xFFE8C37D);
+const whiteColor = Colors.white;
+const blackColor = Colors.black;
+const redColor = Color(0xFFD93A3A);
+
+const accentLightColor = // 10% of accentColor
+const blackLightColor = // 10% of blackColor
+const dividerLight = // 10% of blackColor
+const whiteMuted = // 80% of whiteColor
